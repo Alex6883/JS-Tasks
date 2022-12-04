fbc90d14b2466bc752dc9aa5a245ff838cc96b96
Alex6883 <117719481+Alex6883@users.noreply.github.com>
2022-12-04 20:59:23 +0200

    Add files via upload

---
+++ Task 1/task1.js
@@ -0,0 +1,19 @@
+// Task 1
+let city = "Київ";
+city = "Львів";
+let address = city;
+alert(address);
+
+// Task 2
+let n = 15;
+let result = Math.pow(n, 3);
+console.log(result);
+
+// Task 3
+const a = 4
+const b = 6
+const c = 3.50
+const d = 5.25
+const e = a * d
+const f = c * b
+document.write(e + f)
